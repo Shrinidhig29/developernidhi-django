3a5d9a8d91a3946f5d63bc5692bf166ef2c30f25
Shrinidhi G <65757568+Shrinidhig29@users.noreply.github.com>
2020-12-18 10:27:01 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,7 @@ admin.site.index_title = "Welcome to this Portal"
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about', views.about, name='about'),
     # path('projects', views.projects, name='projects'),
     path('contact', views.contact, name='contact'),
